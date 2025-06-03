b7e704bc14d839c01b0b9269230459506f16a199
YanivShahar <yanivshahar97@gmail.com>
2025-06-03 17:41:44 +0300

    Add Order and OrderItem models for Shoe Store API

---
+++ ShoeAPI/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using ShoeAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoeAPI.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+
+        public string CustomerName { get; set; } = string.Empty;
+        public DateTime OrderDate { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public Order() { }
+    }
+}
+
+
---
+++ ShoeAPI/Models/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeAPI.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public Guid OrderItemId { get; set; }
+
+        public string ProductName { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public int Size { get; set; }
+        public string Color { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public Guid OrderId { get; set; }
+        public Order? Order { get; set; }
+    }
+}
